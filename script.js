b54c04cd981c7f76248a5845eb369bdc91a1edee
Gla3nata <0207natalya@gmail.com>
2025-11-11 23:32:20 +0600

    task1-3

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-function DomElement(selector, height, width, bg, fontSize) {
+const DomElement = function (selector, height, width, bg, fontSize) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
 }
+
+DomElement.prototype.createElement = function(text) {
+    let element;
+
+    if (this.selector.startsWith('.')) {
+        element = document.createElement('div');
+        element.className = this.selector.slice(1);
+    } else if (this.selector.startsWith('#')) {
+        element = document.createElement('p');
+        element.id = this.selector.slice(1);
+    }
+
+    element.style.cssText = `
+        height: ${this.height};
+        width: ${this.width};
+        background: ${this.bg};
+        font-size: ${this.fontSize};
+    `;
+
+    element.textContent = text;
+    document.body.appendChild(element);
+
+    return element;
+};
+
+const myElement = new DomElement('.block', '100px', '200px', 'lightblue', '20px');
+myElement.createElement('какой то текст');
